849aeddaa8cb056e894d199787194b6e6e851728
lineboi <esiomwiz@gmail.com>
2025-05-09 17:18:45 +0400

    MOISE ADDED ALL

---
+++ index.js
@@ -0,0 +1,6 @@
+var cardholder = document.getElementById('cardholder');
+var cardnumber = document.getElementById('cardnumber');
+var month = document.getElementById('month');
+var year = document.getElementById('year');
+var cvc = document.getElementById('cvc');
+var errormsg = document.getElementsByClassName('error');
